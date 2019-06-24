e2a5414a603611f72b19098d60dc1ae65f659c17
DijanaB <dijana.email@gmail.com>
2019-06-24 22:03:15 +0200

    0.1

--- MicroservicePOST/src/main/java/post/MicroservicePostApplication.java
+++ MicroservicePOST/src/main/java/post/MicroservicePostApplication.java
@@ -3,7 +3,9 @@ package post;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients("post")
 @SpringBootApplication
 public class MicroservicePostApplication {
 
